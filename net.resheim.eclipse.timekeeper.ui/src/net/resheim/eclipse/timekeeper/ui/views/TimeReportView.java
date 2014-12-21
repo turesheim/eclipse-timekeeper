38a59b30933bd67be88f6739e6f007cba16e0ef4
Torkild U. Resheim <torkildr@gmail.com>
2014-12-21 21:45:21 +0100

    Fix #15: Add header showing current date and week
    Task-Url: http://github.com/turesheim/eclipse-timekeeper/issues/issue/15

--- net.resheim.eclipse.timekeeper.ui/src/net/resheim/eclipse/timekeeper/ui/views/TimeReportView.java
+++ net.resheim.eclipse.timekeeper.ui/src/net/resheim/eclipse/timekeeper/ui/views/TimeReportView.java
@@ -13,6 +13,7 @@
 package net.resheim.eclipse.timekeeper.ui.views;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.WeekFields;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -61,9 +62,13 @@ import org.eclipse.mylyn.tasks.core.ITaskContainer;
 import org.eclipse.mylyn.tasks.ui.AbstractRepositoryConnectorUi;
 import org.eclipse.mylyn.tasks.ui.TasksUiImages;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.TreeColumn;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.ISharedImages;
@@ -74,267 +79,15 @@ import org.eclipse.ui.part.ViewPart;
 @SuppressWarnings("restriction")
 public class TimeReportView extends ViewPart {
 
-	private final class TaskActivationListener implements ITaskActivationListener {
-		@Override
-		public void taskDeactivated(ITask task) {
-			viewer.setInput(getViewSite());
-		}
-
-		@Override
-		public void taskActivated(ITask task) {
-			viewer.setInput(getViewSite());
-		}
-
-		@Override
-		public void preTaskDeactivated(ITask task) {
-			// Do nothing
-		}
-
-		@Override
-		public void preTaskActivated(ITask task) {
-			// Do nothing
-		}
-	}
-
-	class NameSorter extends ViewerSorter {
-	}
-
-	class ViewContentProvider implements ITreeContentProvider {
-		public void dispose() {
-			TasksUiPlugin.getTaskActivityManager().removeActivationListener(taskActivationListener);
-		}
-
-		public Object[] getElements(Object parent) {
-			Set<String> projects = new HashSet<>();
-			Collection<AbstractTask> filteredTasks = new ArrayList<>();
-			Collection<AbstractTask> allTasks = TasksUiPlugin.getTaskList().getAllTasks();
-			for (AbstractTask task : allTasks) {
-				if (task.getAttribute(TimekeeperPlugin.ATTR_ID) != null) {
-					filteredTasks.add(task);
-				}
-			}
-			for (AbstractTask task : filteredTasks) {
-				projects.add(TimekeeperPlugin.getProjectName(task));
-			}
-			return projects.toArray();
-		}
-
-		public void inputChanged(Viewer v, Object oldInput, Object newInput) {
-			v.refresh();
-		}
-
-		@Override
-		public Object[] getChildren(Object parentElement) {
-			Collection<AbstractTask> filteredTasks = new ArrayList<>();
-			if (parentElement instanceof String) {
-				Collection<AbstractTask> allTasks = TasksUiPlugin.getTaskList().getAllTasks();
-				for (AbstractTask task : allTasks) {
-					if (task.getAttribute(TimekeeperPlugin.ATTR_ID) != null) {
-						if (parentElement.equals(TimekeeperPlugin.getProjectName(task))) {
-							filteredTasks.add(task);
-						}
-					}
-				}
-			}
-			return filteredTasks.toArray();
-		}
-
-		@Override
-		public Object getParent(Object element) {
-			if (element instanceof ITask) {
-				return TimekeeperPlugin.getProjectName((AbstractTask) element);
-			}
-			return null;
-		}
-
-		@Override
-		public boolean hasChildren(Object element) {
-			// Projects are guaranteed to have tasks as children, tasks are
-			// guaranteed to not have any children.
-			if (element instanceof String) {
-				return true;
-			}
-			return false;
-		}
-	}
-
-	public static final String ID = "net.resheim.eclipse.timekeeper.ui.views.SampleView";
-
-	private Action action1;
-
-	private Action action2;
-
-	private Action doubleClickAction;
-
-	private TreeViewer viewer;
-
-	private TaskActivationListener taskActivationListener;
-
-	/**
-	 * The constructor.
-	 */
-	public TimeReportView() {
-		// Determine the first date of the week
-		LocalDate date = LocalDate.now();
-		WeekFields weekFields = WeekFields.of(Locale.getDefault());
-		int day = date.get(weekFields.dayOfWeek());
-		firstDayOfWeek = date.minusDays(day - 1);
-	}
-
-	private void contributeToActionBars() {
-		IActionBars bars = getViewSite().getActionBars();
-		fillLocalPullDown(bars.getMenuManager());
-		fillLocalToolBar(bars.getToolBarManager());
-	}
-
-	@Override
-	public void createPartControl(Composite parent) {
-		viewer = new TreeViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL | SWT.FULL_SELECTION);
-		viewer.setContentProvider(new ViewContentProvider());
-
-		createTitleColumn();
-		String[] headings = Activator.getDefault().getHeadings(LocalDate.now());
-		for (int i = 0; i < 7; i++) {
-			createTimeColumn(i, headings[i]);
-		}
-
-		viewer.setSorter(new NameSorter());
-		viewer.setInput(getViewSite());
-
-		viewer.getTree().setHeaderVisible(true);
-
-		makeActions();
-		hookContextMenu();
-		hookDoubleClickAction();
-		contributeToActionBars();
-		taskActivationListener = new TaskActivationListener();
-		TasksUiPlugin.getTaskActivityManager().addActivationListener(taskActivationListener);
-	}
-
-	private TreeViewerColumn createTableViewerColumn(String title, int bound, final int colNumber) {
-		final TreeViewerColumn viewerColumn = new TreeViewerColumn(viewer, SWT.NONE);
-		TreeColumn column = viewerColumn.getColumn();
-		column.setText(title);
-		column.setWidth(bound);
-		column.setResizable(true);
-		column.setMoveable(true);
-		return viewerColumn;
-	}
-
-	private class TimeEditingSupport extends EditingSupport {
-
-		int weekday;
-
-		public TimeEditingSupport(ColumnViewer viewer, int weekday) {
-			super(viewer);
-			this.weekday = weekday;
-		}
-
-		@Override
-		protected CellEditor getCellEditor(Object element) {
-			return new TextCellEditor(viewer.getTree());
-		}
-
-		@Override
-		protected boolean canEdit(Object element) {
-			if (element instanceof ITask) {
-				return true;
-			}
-			return false;
-		}
-
-		@Override
-		protected Object getValue(Object element) {
-			if (element instanceof ITask) {
-				AbstractTask task = (AbstractTask) element;
-				int seconds = TimekeeperPlugin.getIntValue(task, getDateString(weekday));
-				if (seconds > 0) {
-					return DurationFormatUtils.formatDuration(seconds * 1000, "H:mm", true);
-				}
-				return "0:00";
-			}
-			return "";
-		}
-
-		@Override
-		protected void setValue(Object element, Object value) {
-			if (element instanceof ITask) {
-				ITask task = (ITask) element;
-				if (value instanceof String) {
-					String[] split = ((String) value).split(":");
-					// Only minutes are given
-					if (split.length == 1) {
-						int newValue = Integer.parseInt(split[0]) * 60;
-						TimekeeperPlugin.setValue(task, getDateString(weekday), Integer.toString(newValue));
-					}
-					if (split.length == 2) {
-						int newValue = Integer.parseInt(split[0]) * 3600 + Integer.parseInt(split[1]) * 60;
-						TimekeeperPlugin.setValue(task, getDateString(weekday), Integer.toString(newValue));
-					}
-					viewer.update(element, null);
-				}
-			}
-		}
-
-	}
-
-	private void createTimeColumn(int weekday, String title) {
-		TreeViewerColumn keyColumn = createTableViewerColumn(title, 50, 1 + weekday);
-		keyColumn.setEditingSupport(new TimeEditingSupport(keyColumn.getViewer(), weekday));
-		keyColumn.setLabelProvider(new ColumnLabelProvider() {
-
-			@Override
-			public String getText(Object element) {
-				if (element instanceof String) {
-					// TODO: Return the sum for the project
-					return "";
-				}
-				AbstractTask task = (AbstractTask) element;
-				int seconds = TimekeeperPlugin.getIntValue(task, getDateString(weekday));
-				if (seconds > 0) {
-					return DurationFormatUtils.formatDuration(seconds * 1000, "H:mm", true);
-				}
-				return "";
-			}
-		});
-	}
-
-	private final LocalDate firstDayOfWeek;
-
-	/**
-	 * Returns a string representation of the date.
-	 *
-	 * @param weekday
-	 * @return
-	 */
-	private String getDateString(int weekday) {
-		return firstDayOfWeek.plusDays(weekday).toString();
-	}
-
 	private class CompositeImageDescriptor {
 
 		ImageDescriptor icon;
 
 		ImageDescriptor overlayKind;
 
-	};
+	}
 
 	private class LP extends ColumnLabelProvider {
-		@Override
-		public String getText(Object element) {
-			if (element instanceof String) {
-				return (String) element;
-			}
-			ITask task = ((ITask) element);
-			StringBuilder sb = new StringBuilder();
-			if (task.getTaskKey() != null) {
-				sb.append(task.getTaskKey());
-				sb.append(": ");
-			}
-			sb.append(task.getSummary());
-			return sb.toString();
-		}
-
 		@Override
 		public Image getImage(Object element) {
 			CompositeImageDescriptor compositeDescriptor = getImageDescriptor(element);
@@ -390,7 +143,272 @@ public class TimeReportView extends ViewPart {
 			return compositeDescriptor;
 		}
 
+		@Override
+		public String getText(Object element) {
+			if (element instanceof String) {
+				return (String) element;
+			}
+			ITask task = ((ITask) element);
+			StringBuilder sb = new StringBuilder();
+			if (task.getTaskKey() != null) {
+				sb.append(task.getTaskKey());
+				sb.append(": ");
+			}
+			sb.append(task.getSummary());
+			return sb.toString();
+		}
+
 	}
+
+	class NameSorter extends ViewerSorter {
+	}
+
+	private final class TaskActivationListener implements ITaskActivationListener {
+		@Override
+		public void preTaskActivated(ITask task) {
+			// Do nothing
+		}
+
+		@Override
+		public void preTaskDeactivated(ITask task) {
+			// Do nothing
+		}
+
+		@Override
+		public void taskActivated(ITask task) {
+			viewer.update(task, null);
+		}
+
+		@Override
+		public void taskDeactivated(ITask task) {
+			viewer.update(task, null);
+		}
+	}
+
+	private class TimeEditingSupport extends EditingSupport {
+
+		int weekday;
+
+		public TimeEditingSupport(ColumnViewer viewer, int weekday) {
+			super(viewer);
+			this.weekday = weekday;
+		}
+
+		@Override
+		protected boolean canEdit(Object element) {
+			if (element instanceof ITask) {
+				return true;
+			}
+			return false;
+		}
+
+		@Override
+		protected CellEditor getCellEditor(Object element) {
+			return new TextCellEditor(viewer.getTree());
+		}
+
+		@Override
+		protected Object getValue(Object element) {
+			if (element instanceof ITask) {
+				AbstractTask task = (AbstractTask) element;
+				int seconds = TimekeeperPlugin.getIntValue(task, getDateString(weekday));
+				if (seconds > 0) {
+					return DurationFormatUtils.formatDuration(seconds * 1000, "H:mm", true);
+				}
+				return "0:00";
+			}
+			return "";
+		}
+
+		@Override
+		protected void setValue(Object element, Object value) {
+			if (element instanceof ITask) {
+				ITask task = (ITask) element;
+				if (value instanceof String) {
+					String[] split = ((String) value).split(":");
+					// Only minutes are given
+					if (split.length == 1) {
+						int newValue = Integer.parseInt(split[0]) * 60;
+						TimekeeperPlugin.setValue(task, getDateString(weekday), Integer.toString(newValue));
+					}
+					if (split.length == 2) {
+						int newValue = Integer.parseInt(split[0]) * 3600 + Integer.parseInt(split[1]) * 60;
+						TimekeeperPlugin.setValue(task, getDateString(weekday), Integer.toString(newValue));
+					}
+					viewer.update(element, null);
+				}
+			}
+		}
+
+	}
+
+	class ViewContentProvider implements ITreeContentProvider {
+		public void dispose() {
+			TasksUiPlugin.getTaskActivityManager().removeActivationListener(taskActivationListener);
+		}
+
+		@Override
+		public Object[] getChildren(Object parentElement) {
+			Collection<AbstractTask> filteredTasks = new ArrayList<>();
+			if (parentElement instanceof String) {
+				Collection<AbstractTask> allTasks = TasksUiPlugin.getTaskList().getAllTasks();
+				for (AbstractTask task : allTasks) {
+					if (task.getAttribute(TimekeeperPlugin.ATTR_ID) != null) {
+						if (parentElement.equals(TimekeeperPlugin.getProjectName(task))) {
+							filteredTasks.add(task);
+						}
+					}
+				}
+			}
+			return filteredTasks.toArray();
+		}
+
+		public Object[] getElements(Object parent) {
+			Set<String> projects = new HashSet<>();
+			Collection<AbstractTask> filteredTasks = new ArrayList<>();
+			Collection<AbstractTask> allTasks = TasksUiPlugin.getTaskList().getAllTasks();
+			for (AbstractTask task : allTasks) {
+				if (task.getAttribute(TimekeeperPlugin.ATTR_ID) != null) {
+					filteredTasks.add(task);
+				}
+			}
+			for (AbstractTask task : filteredTasks) {
+				projects.add(TimekeeperPlugin.getProjectName(task));
+			}
+			return projects.toArray();
+		}
+
+		@Override
+		public Object getParent(Object element) {
+			if (element instanceof ITask) {
+				return TimekeeperPlugin.getProjectName((AbstractTask) element);
+			}
+			return null;
+		}
+
+		@Override
+		public boolean hasChildren(Object element) {
+			// Projects are guaranteed to have tasks as children, tasks are
+			// guaranteed to not have any children.
+			if (element instanceof String) {
+				return true;
+			}
+			return false;
+		}
+
+		public void inputChanged(Viewer v, Object oldInput, Object newInput) {
+			v.refresh();
+		}
+	}
+
+	public static final String ID = "net.resheim.eclipse.timekeeper.ui.views.SampleView";
+
+	private Action action1;
+
+	private Action action2;
+
+	private Action doubleClickAction;
+
+	private final LocalDate firstDayOfWeek;
+
+	private TaskActivationListener taskActivationListener;
+
+	private TreeViewer viewer;
+
+	/**
+	 * The constructor.
+	 */
+	public TimeReportView() {
+		// Determine the first date of the week
+		LocalDate date = LocalDate.now();
+		WeekFields weekFields = WeekFields.of(Locale.getDefault());
+		int day = date.get(weekFields.dayOfWeek());
+		firstDayOfWeek = date.minusDays(day - 1);
+	}
+
+	private void contributeToActionBars() {
+		IActionBars bars = getViewSite().getActionBars();
+		fillLocalPullDown(bars.getMenuManager());
+		fillLocalToolBar(bars.getToolBarManager());
+	}
+
+	private static final DateTimeFormatter weekFormat = DateTimeFormatter.ofPattern("w");
+	private static final DateTimeFormatter dateFormat = DateTimeFormatter.ISO_LOCAL_DATE;
+
+	@Override
+	public void createPartControl(Composite parent) {
+		ScrolledComposite root = new ScrolledComposite(parent, SWT.V_SCROLL);
+		root.setBackground(parent.getBackground());
+		root.setExpandHorizontal(true);
+		root.setExpandVertical(true);
+
+		Composite main = new Composite(root, SWT.NONE);
+		main.setBackground(parent.getBackground());
+		main.setLayout(new GridLayout());
+		root.setContent(main);
+
+		Text dateTimeLabel = new Text(main, SWT.NONE);
+		dateTimeLabel.setLayoutData(new GridData());
+		StringBuilder sb = new StringBuilder();
+		sb.append("Showing week ");
+		sb.append(firstDayOfWeek.format(weekFormat));
+		sb.append(" starting at ");
+		sb.append(firstDayOfWeek.format(dateFormat));
+		dateTimeLabel.setText(sb.toString());
+		viewer = new TreeViewer(main, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL | SWT.FULL_SELECTION);
+		viewer.setContentProvider(new ViewContentProvider());
+		GridData layoutData = new GridData(SWT.FILL, SWT.FILL, true, true);
+		viewer.getControl().setLayoutData(layoutData);
+
+		createTitleColumn();
+		String[] headings = Activator.getDefault().getHeadings(LocalDate.now());
+		for (int i = 0; i < 7; i++) {
+			createTimeColumn(i, headings[i]);
+		}
+
+		viewer.setSorter(new NameSorter());
+		viewer.setInput(getViewSite());
+
+		viewer.getTree().setHeaderVisible(true);
+
+		makeActions();
+		hookContextMenu();
+		hookDoubleClickAction();
+		contributeToActionBars();
+		taskActivationListener = new TaskActivationListener();
+		TasksUiPlugin.getTaskActivityManager().addActivationListener(taskActivationListener);
+	}
+
+	private TreeViewerColumn createTableViewerColumn(String title, int bound, final int colNumber) {
+		final TreeViewerColumn viewerColumn = new TreeViewerColumn(viewer, SWT.NONE);
+		TreeColumn column = viewerColumn.getColumn();
+		column.setText(title);
+		column.setWidth(bound);
+		column.setResizable(true);
+		column.setMoveable(true);
+		return viewerColumn;
+	}
+
+	private void createTimeColumn(int weekday, String title) {
+		TreeViewerColumn keyColumn = createTableViewerColumn(title, 50, 1 + weekday);
+		keyColumn.setEditingSupport(new TimeEditingSupport(keyColumn.getViewer(), weekday));
+		keyColumn.setLabelProvider(new ColumnLabelProvider() {
+
+			@Override
+			public String getText(Object element) {
+				if (element instanceof String) {
+					// TODO: Return the sum for the project
+					return "";
+				}
+				AbstractTask task = (AbstractTask) element;
+				int seconds = TimekeeperPlugin.getIntValue(task, getDateString(weekday));
+				if (seconds > 0) {
+					return DurationFormatUtils.formatDuration(seconds * 1000, "H:mm", true);
+				}
+				return "";
+			}
+		});
+	};
 
 	private void createTitleColumn() {
 		TreeViewerColumn keyColumn = createTableViewerColumn("Activity", 400, 1);
@@ -413,6 +431,16 @@ public class TimeReportView extends ViewPart {
 	private void fillLocalToolBar(IToolBarManager manager) {
 		manager.add(action1);
 		manager.add(action2);
+	}
+
+	/**
+	 * Returns a string representation of the date.
+	 *
+	 * @param weekday
+	 * @return
+	 */
+	private String getDateString(int weekday) {
+		return firstDayOfWeek.plusDays(weekday).toString();
 	}
 
 	private void hookContextMenu() {
