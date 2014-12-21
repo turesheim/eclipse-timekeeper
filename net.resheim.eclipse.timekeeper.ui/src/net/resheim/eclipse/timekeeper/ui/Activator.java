a7fae98fa559f5772d86737d307680e9026e249d
Torkild U. Resheim <torkildr@gmail.com>
2014-12-21 13:54:09 +0100

    Improve weekday formatting.

--- net.resheim.eclipse.timekeeper.ui/src/net/resheim/eclipse/timekeeper/ui/Activator.java
+++ net.resheim.eclipse.timekeeper.ui/src/net/resheim/eclipse/timekeeper/ui/Activator.java
@@ -19,7 +19,7 @@ public class Activator extends AbstractUIPlugin {
 
 	// The shared instance
 	private static Activator plugin;
-	
+
 	/**
 	 * The constructor
 	 */
@@ -28,8 +28,12 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
 	 */
+	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
@@ -37,8 +41,12 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
 	 */
+	@Override
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
@@ -54,30 +62,39 @@ public class Activator extends AbstractUIPlugin {
 	}
 
 	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path
+	 * Returns an image descriptor for the image file at the given plug-in
+	 * relative path
 	 *
-	 * @param path the path
+	 * @param path
+	 *            the path
 	 * @return the image descriptor
 	 */
 	public static ImageDescriptor getImageDescriptor(String path) {
 		return imageDescriptorFromPlugin(PLUGIN_ID, path);
 	}
-	
-	private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE d");;
 
-	public String[] getHeadings(LocalDate date){
+	private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE d").withLocale(Locale.US);
+
+	/**
+	 * Returns a list of weekday names and dates, using the default locale to
+	 * determine the first day of the week (Sunday or Monday).
+	 *
+	 * @param date
+	 *            starting date
+	 * @return an array with weekday names and dates
+	 */
+	public String[] getHeadings(LocalDate date) {
 		String[] headings = new String[7];
 		WeekFields weekFields = WeekFields.of(Locale.getDefault());
 		// Current day in the week
 		int day = date.get(weekFields.dayOfWeek());
 		// First date of the week
 		LocalDate first = date.minusDays(day - 1);
-		for (int i=0;i<7;i++){
+		for (int i = 0; i < 7; i++) {
 			headings[i] = formatter.format(first);
 			first = first.plusDays(1);
 		}
 		return headings;
 	}
-	
+
 }
