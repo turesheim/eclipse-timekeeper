b44dd81805753853ec2b56a73378fd3282be30d2
Torkild U. Resheim <torkildr@gmail.com>
2015-08-13 14:18:28 +0200

    Fix #69: Improve restart handling
    Removed the feature.
    
    Task-Url: http://github.com/turesheim/eclipse-timekeeper/issues/issue/69

--- README.md
+++ README.md
@@ -6,8 +6,6 @@ This is a simple time-tracking plug-in integrating with [Eclipse Mylyn](http://e
 
 Whenever a task is *activated* in Mylyn it will automatically show up in the **Workweek** view with a bold label, and the amount of time the task is active will be tracked. When the task is *deactivated* the active time is added to the total for the task on the particular day. It is also possible to manually edit the time by clicking into the cell. If a number is typed, such as *45*, it will be translated into minutes and show up as *0:45*. If a decimal number is entered, for example *1.5*, it will be translated to *1:30*. The decimal separator can be either "," or ".".
 
-If a task is active when Eclipse is closed you will be asked whether or not to add the duration in between when Eclipse is started again. If you answer **OK** the time will be added to the total for the starting date. If you answer **Cancel** no time is added.
-
 The context menu and toolbar buttons can be used to browse back and forward by one week. The current locale is used to determine week numbers. Left of the navigation buttons there is a button for copying the workweek in HTML format to the clipboard.
 
 The task context menu contains an entry **Copy Details to HTML**. This will copy the link and summary details as HTML to the clipboard.
@@ -33,12 +31,12 @@ You can install from the <a href="http://marketplace.eclipse.org/content/timekee
 Clone the project and from the root execute:
 
     mvn clean verify
-    
+
 When successful there will be a Eclipse p2 repository at *net.resheim.eclipse.timekeeper-site/target/repository* which you can install from.
 
 ## Note
 
-This project started out as an experiment, attempting to make use of the *Java 8 Date/Time API* along with new collection features such as *Streams*. Hence **Java 8** is absolutely required to execute this bundle. 
+This project started out as an experiment, attempting to make use of the *Java 8 Date/Time API* along with new collection features such as *Streams*. Hence **Java 8** is absolutely required to execute this bundle.
 
 ## License
 
--- net.resheim.eclipse.timekeeper.ui/src/net/resheim/eclipse/timekeeper/internal/TaskActivationListener.java
+++ net.resheim.eclipse.timekeeper.ui/src/net/resheim/eclipse/timekeeper/internal/TaskActivationListener.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 Torkild U. Resheim.
+ * Copyright (c) 2014-2015 Torkild U. Resheim.
  *
  * All rights reserved. This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License v1.0 which
@@ -13,16 +13,10 @@ package net.resheim.eclipse.timekeeper.internal;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
-import java.util.Locale;
 
-import org.apache.commons.lang.time.DurationFormatUtils;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.mylyn.tasks.core.ITask;
 import org.eclipse.mylyn.tasks.core.ITaskActivationListener;
-import org.eclipse.swt.widgets.Display;
 
 import net.resheim.eclipse.timekeeper.ui.Activator;
 
@@ -37,23 +31,6 @@ public class TaskActivationListener implements ITaskActivationListener {
 	@Override
 	public void preTaskActivated(ITask task) {
 		LocalDateTime now = LocalDateTime.now();
-		String startString = Activator.getValue(task, Activator.START);
-		String tickString = Activator.getValue(task, Activator.TICK);
-		if (startString != null) {
-			LocalDateTime ticked = LocalDateTime.parse(tickString);
-			LocalDateTime stopped = LocalDateTime.now();
-			long seconds = ticked.until(stopped, ChronoUnit.SECONDS);
-			String time = DurationFormatUtils.formatDuration(seconds * 1000, "H:mm", true);
-			boolean confirm = MessageDialog.openConfirm(
-					Display.getCurrent().getActiveShell(),
-					"Add elapsed time?",
-					"Work was already started and task was last updated on "
-							+ ticked.format(DateTimeFormatter.ofPattern("EEE e, HH:mm", Locale.US))
-							+ ". Continue and add the elapsed time since (" + time + ") to the task total?");
-			if (confirm) {
-				Activator.accumulateTime(task, startString, ticked.until(LocalDateTime.now(), ChronoUnit.MILLIS));
-			}
-		}
 		Activator.setValue(task, Activator.TICK, now.toString());
 		Activator.setValue(task, Activator.START, now.toString());
 	}
