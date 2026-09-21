65fe25dcfe4f8d5d95bcb88b5b0aeed98ed75c7b
Torkild U. Resheim <tur@itema.no>
2026-09-21 06:29:09 +0200

    Use SLF4J for freshly compiled SWTBot tests

--- net.resheim.eclipse.timekeeper.test/src/main/java/net/resheim/eclipse/timekeeper/ui/test/IntegrationTest.java
+++ net.resheim.eclipse.timekeeper.test/src/main/java/net/resheim/eclipse/timekeeper/ui/test/IntegrationTest.java
@@ -20,8 +20,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.mylyn.internal.tasks.core.TaskList;
 import org.eclipse.mylyn.internal.tasks.ui.TasksUiPlugin;
@@ -60,9 +60,8 @@ import net.resheim.eclipse.timekeeper.db.model.Task;
 public class IntegrationTest {
 	
 	
-	private static final Logger log = Logger.getLogger(IntegrationTest.class);
+	private static final Logger log = LoggerFactory.getLogger(IntegrationTest.class);
 	static {
-		BasicConfigurator.configure();
 		SWTBotPreferences.TIMEOUT = 20000;
 	}
 
